3fc134744a12308be6fa35dbd9b0abc8ba5d48f2
KarenNgala <ngalalalu@gmail.com>
2020-09-22 21:36:24 +0300

    feature(ft-modelling): project backend - create models - create tests - create apis
    Models

--- backend/tests.py
+++ backend/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from datetime import datetime
+import os
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.contrib.auth.models import User
+from .models import *
 
-# Create your tests here.
+
+class ProfileTest(TestCase):
+    ''' test class for Profile model'''
+    def setUp(self):
+        ''' method called before each test case'''
+        self.user = User.objects.create_user(username='Water')
+        self.interest = Interest(name='IT')
+        self.interest.save()
+
+    def tearDown(self):
+        ''' method to clear all setup instances after each test run '''
+        self.user.delete()
+
+    def test_profile_creation(self):
+        ''' method to test profile instance is created only once for each user '''
+        self.assertIsInstance(self.user.profile, Profile)
+        self.user.save()
+        self.assertIsInstance(self.user.profile, Profile)
